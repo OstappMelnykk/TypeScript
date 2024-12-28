9f0df54c12691bcf859e2615c3726b1236ece0bd
OstappMelnykk <79ostap@ukr.net>
2024-12-28 06:58:00 +0200

    Own types

---
+++ src/OwnTypes.ts
@@ -0,0 +1,25 @@
+type UserType = {
+    name:string,
+    age: number,
+    address: AddressType
+}
+type AddressType = {
+    city: string
+    country: string
+}
+
+let myUser: UserType;
+let myAddress: AddressType;
+
+myAddress = {
+    city: "Lviv",
+    country: "Ukraine"
+}
+
+myUser = {
+    name: "ostap",
+    age: 24,
+    address: myAddress
+}
+
+
